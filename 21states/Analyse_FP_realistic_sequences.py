e68c698a0e1b85cfd52f913816eeb34bb94b351a
ndietler <85280172+ndietler@users.noreply.github.com>
2022-09-21 14:16:38 +0200

    added comments

--- 21states/Analyse_FP_realistic_sequences.py
+++ 21states/Analyse_FP_realistic_sequences.py
@@ -146,15 +146,18 @@ pfam_to_pdb = {"PF00072": {"pdb": {"id": "3ilh",
                                     "idxs": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109]}}}
 
 
-
+####################### MAIN #######################
+##path to the sequences dataset generated with on the inferred tree. Here is an example dataset based on the PF72 family.
 path_tree = './data_example/Tree_data/sequences/PF00072_0.pkl'
 tree = FPanalysisPhyloData(path_tree)
 size_seq = int(tree.clade.comment.shape[0])
 dict_seq = {}
 DictionarySequences(tree.clade,dict_seq,size_seq)
+##path folder to the contacts maps. Here is the example to the experimental one of PF72 and the bmDCA inferred contact map of the same family.
 path_folder_contactmaps = './data_example/Tree_data/contactmaps/'
 keys = ["", "bmDCA", "bmDCA_FastTree_eq"]
 msaname = 'PF00072'
+##path to the example score, here the score is inferred by MI on the tree dataset of PF72.
 path_score = './data_example/Tree_data/scores/PF00072_msa_phylo_equi_bmdca_tree_0_MI_scores.npy'
 scores,bm_cm, pdb_cm, seq_prox_mask = OpenDataNew(msaname, keys[0],path_score,pfam_to_pdb,path_folder_contactmaps)
 number_contacts_bm = np.sum(bm_cm)
